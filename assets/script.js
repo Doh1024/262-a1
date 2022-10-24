f83582a112eaf974284b906cf1ad72f81bed2c87
Doh1024 <dohyeongkim001024@gmail.com>
2022-10-24 15:58:20 -0500

    deleted things in css file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,16 @@
+//global variables 
 const wordArr = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit", "Accusamus", "pariatur", "nobis", "rem", "inventore", "in", "nostrum", "soluta", "eius", "eveniet", "qui", "ea"];
 const button = document.querySelector('button');
 
-console.log(button);
 
 const newFunc = () => {
+  //getting random index from the array
   let item = wordArr[Math.floor(Math.random()*wordArr.length-1)];
   let newItem = document.getElementById('content');
+  //user input
   let input = prompt("Type a word you want to display. (random word if you don't type anything)");
 
+  //if no input or cancelled display random word from the array if not, display the user input
   if(input != "" && input != null){
     item = input;
     newItem.innerHTML = item;
@@ -17,5 +20,5 @@ const newFunc = () => {
   }
   console.log(item);
 }
-
+//eventlistener
 button.addEventListener ('click', newFunc);
